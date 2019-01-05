5f89c1e82ff967eda22b1718bb6b8951198205c6
Petr Vnenk <petr.vnenk@gmail.com>
2019-01-05 13:13:11 +0700

    Fix test for game controller

--- src/modules/game/game.controller.spec.ts
+++ src/modules/game/game.controller.spec.ts
@@ -1,15 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GameController } from './game.controller';
+import { GameService } from './game.service';
 
 describe('Game Controller', () => {
   let module: TestingModule;
   beforeAll(async () => {
     module = await Test.createTestingModule({
       controllers: [GameController],
+      providers: [GameService],
     }).compile();
   });
   it('should be defined', () => {
-    const controller: GameController = module.get<GameController>(GameController);
+    const controller: GameController = module.get<GameController>(
+      GameController,
+    );
     expect(controller).toBeDefined();
   });
 });
