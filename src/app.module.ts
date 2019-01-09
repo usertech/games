204e387dcaf062c0004776408d4a1793a167f602
Petr Vnenk <petr.vnenk@gmail.com>
2019-01-09 15:44:21 +0700

    Consolidate the main app module and bootstrap

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
-import { GameModule } from 'modules/game/game.module';
-import { ConfigModule } from 'modules/config/config.module';
+import { GameModule } from './modules/game/game.module';
+import { ConfigModule } from './modules/config/config.module';
+import { CallModule } from './modules/call/call.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [GameModule, ConfigModule],
+  imports: [
+    ConfigModule,
+    CallModule,
+    GameModule,
+    TypeOrmModule.forRoot({
+      type: 'sqljs',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'app.module';
+import { ConfigService } from 'modules/config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get<ConfigService>(ConfigService);
+  await app.listen(config.port);
 }
 bootstrap();
